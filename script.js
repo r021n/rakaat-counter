919a67ad3ce3cff1a248925897950babe238adde
r021n <rozinabdul25@gmail.com>
2024-12-30 17:33:33 +0700

    modifikasi beberapa kode

--- script.js
+++ script.js
@@ -25,112 +25,6 @@ const initializefaceDetector = async () => {
 };
 initializefaceDetector();
 
-/********************************************************************
-// Demo 1: Grab a bunch of images from the page and detection them
-// upon click.
-********************************************************************/
-
-const imageContainers = document.getElementsByClassName("detectOnClick");
-
-for (let imageContainer of imageContainers) {
-    imageContainer.children[0].addEventListener("click", handleClick);
-}
-
-/**
- * Detect faces in still images on click
- */
-async function handleClick(event) {
-    const highlighters = event.target.parentNode.getElementsByClassName(
-        "highlighter"
-    );
-    while (highlighters[0]) {
-        highlighters[0].parentNode.removeChild(highlighters[0]);
-    }
-
-    const infos = event.target.parentNode.getElementsByClassName("info");
-    while (infos[0]) {
-        infos[0].parentNode.removeChild(infos[0]);
-    }
-    const keyPoints = event.target.parentNode.getElementsByClassName("key-point");
-    while (keyPoints[0]) {
-        keyPoints[0].parentNode.removeChild(keyPoints[0]);
-    }
-
-    if (!faceDetector) {
-        console.log("Wait for objectDetector to load before clicking");
-        return;
-    }
-
-    // if video mode is initialized, set runningMode to image
-    if (runningMode === "VIDEO") {
-        runningMode = "IMAGE";
-        await faceDetector.setOptions({ runningMode: "IMAGE" });
-    }
-
-    const ratio = event.target.height / event.target.naturalHeight;
-
-    // faceDetector.detect returns a promise which, when resolved, is an array of Detection faces
-    const detections = faceDetector.detect(event.target).detections;
-    console.log(detections);
-
-    displayImageDetections(detections, event.target);
-}
-
-function displayImageDetections(detections, resultElement) {
-    const ratio = resultElement.height / resultElement.naturalHeight;
-    console.log(ratio);
-
-    for (let detection of detections) {
-        // Description text
-        const p = document.createElement("p");
-        p.setAttribute("class", "info");
-        p.innerText =
-            "Confidence: " +
-            Math.round(parseFloat(detection.categories[0].score) * 100) +
-            "% .";
-        // Positioned at the top left of the bounding box.
-        // Height is whatever the text takes up.
-        // Width subtracts text padding in CSS so fits perfectly.
-        p.style =
-            "left: " +
-            detection.boundingBox.originX * ratio +
-            "px;" +
-            "top: " +
-            (detection.boundingBox.originY * ratio - 30) +
-            "px; " +
-            "width: " +
-            (detection.boundingBox.width * ratio - 10) +
-            "px;" +
-            "hight: " +
-            20 +
-            "px;";
-        const highlighter = document.createElement("div");
-        highlighter.setAttribute("class", "highlighter");
-        highlighter.style =
-            "left: " +
-            detection.boundingBox.originX * ratio +
-            "px;" +
-            "top: " +
-            detection.boundingBox.originY * ratio +
-            "px;" +
-            "width: " +
-            detection.boundingBox.width * ratio +
-            "px;" +
-            "height: " +
-            detection.boundingBox.height * ratio +
-            "px;";
-
-        resultElement.parentNode.appendChild(highlighter);
-        resultElement.parentNode.appendChild(p);
-        for (let keypoint of detection.keypoints) {
-            const keypointEl = document.createElement("spam");
-            keypointEl.className = "key-point";
-            keypointEl.style.top = `${keypoint.y * resultElement.height - 3}px`;
-            keypointEl.style.left = `${keypoint.x * resultElement.width - 3}px`;
-            resultElement.parentNode.appendChild(keypointEl);
-        }
-    }
-}
 
 /********************************************************************
 // Demo 2: Continuously grab image from webcam stream and detect it.
@@ -256,16 +150,26 @@ function displayVideoDetections(detections) {
         // Store drawn objects in memory so they are queued to delete at next call
         children.push(highlighter);
         children.push(p);
-        for (let keypoint of detection.keypoints) {
+        // for (let keypoint of detection.keypoints) {
+        //     const keypointEl = document.createElement("spam");
+        //     keypointEl.className = "key-point";
+        //     keypointEl.style.top = `${keypoint.y * video.offsetHeight - 3}px`;
+        //     keypointEl.style.left = `${video.offsetWidth - keypoint.x * video.offsetWidth - 3
+        //         }px`;
+        //     liveView.appendChild(keypointEl);
+        //     children.push(keypointEl);
+        // }
+        for (let i = 0; i < detection.keypoints.length; i++) {
+            const keypoint = detection.keypoints[i];
             const keypointEl = document.createElement("spam");
-            keypointEl.className = "key-point";
+            keypointEl.className = (i === 2) ? "key-point" : "key-dot";
             keypointEl.style.top = `${keypoint.y * video.offsetHeight - 3}px`;
-            keypointEl.style.left = `${video.offsetWidth - keypoint.x * video.offsetWidth - 3
-                }px`;
+            keypointEl.style.left = `${video.offsetWidth - keypoint.x * video.offsetWidth - 3}px`;
             liveView.appendChild(keypointEl);
             children.push(keypointEl);
         }
+
+        console.log(`X = ${detection.keypoints[2].y * video.offsetHeight - 3}, X = ${video.offsetWidth - detection.keypoints[2].x * video.offsetWidth - 3}`)
+        console.log(detection.keypoints)
     }
 }
-
-console.log('page ready')
